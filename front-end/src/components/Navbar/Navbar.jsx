04e1f00fcc741d4fc043666367f3d2ccb90391c7
avytran <tr.anhvy.work@gmail.com>
2025-04-18 18:30:57 +0700

    style(WDA-32): style main layout

--- front-end/src/components/Navbar/Navbar.jsx
+++ front-end/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Navbar.css";
 import { NavLink } from 'react-router-dom';
 import { navbarContents } from '../../constants';
-import { logo } from '../../assets/images/navbar-icons';
+import { logo, logout } from '../../assets/images/navbar-icons';
 
 export const Navbar = () => {
   return (
@@ -20,13 +20,19 @@ export const Navbar = () => {
           })
         }
       </div>
+      <NavbarItem
+        className="navbar-logout"
+        to="/logout"
+        label="Đăng xuất"
+        icon={logout}
+      />
     </nav>
   )
 }
 
-const NavbarItem = ({ to, label, icon }) => {
+const NavbarItem = ({ to, label, icon, className }) => {
   return (
-    <NavLink to={to} className={({ isActive }) => isActive ? 'active-link navbar-item' : 'navbar-item'}>
+    <NavLink to={to} className={`navbar-item ${className || ""} ${({ isActive }) => isActive ? 'active-link' : 'navbar-item'}`}>
       <img src={icon} alt="" />
       <p>{label}</p>
     </NavLink>
