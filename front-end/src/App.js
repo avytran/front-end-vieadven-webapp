02f33f050fa828b1541bdf59731bb1028f13549d
Anh Vy Tran <tr.anhvy.work@gmail.com>
2025-04-27 12:00:43 +0700

    Feature/WDA-34 Build Card for Leaderboards (#6)
    * feat(WDA-34): build card for Leaderboards and add necessary resource for Leaderboards
    
    * feat(WDA-34): add service
    
    ---------
    
    Co-authored-by: ArthurHoang15 <hoangthianhquan2005@gmail.com>

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { MainLayout } from './layouts/MainLayout';
+import { Leaderboard } from './components/Leaderboard';
 import { HomePage } from './pages/HomePage';
 import { MissionPage } from './pages/MissionPage';
 
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route element={<MainLayout />}>
               <Route path="/" element={<HomePage />} />
+              <Route path="/leaderboard" element={<Leaderboard />} />
               <Route path="/mission" element={<MissionPage />} />
             </Route>
           </Routes>
