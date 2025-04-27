8b93ba6fa2f32f63b725265c163e49da1fe0de22
Anh Vy Tran <tr.anhvy.work@gmail.com>
2025-04-27 10:48:53 +0700

    Feature/WDA-59 Build Mission Screen (#5)
    * feat(WDA-59): add mission screen
    
    * feat(WDA-59): add service
    
    * style(WDA-59): adjust css

--- front-end/src/components/Item/Item.jsx
+++ front-end/src/components/Item/Item.jsx
@@ -4,8 +4,10 @@ import './Item.css'
 export const Item = ({ item }) => {
     return(
         <div className="item-container">
-            <img className="item-icon" src={item?.icon_url} alt="" />
-            <span>{item?.quantity}</span>
+            <div className="icon-container">
+                <img className="item-icon" src={item.icon_url} alt="" />
+            </div>
+            <span>{item.quantity}</span>
         </div>
     )
 }
